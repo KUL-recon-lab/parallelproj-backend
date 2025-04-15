eaf4c62e7ddd8f2f56e75c6bf374c9394a1eebea
Georg Schramm <georg.schramm@kuleuven.be>
2025-04-15 14:49:24 +0200

    add pytestmark

---
+++ tests/config.py
@@ -0,0 +1,25 @@
+import pytest
+
+import array_api_strict as nparr
+import array_api_compat.numpy as np
+
+# generate list of array_api modules / device combinations to test
+
+xp_dev_list = []
+xp_dev_list.append((np, "cpu"))
+xp_dev_list.append((nparr, None))
+
+# if parallelproj.cupy_enabled:
+#    import array_api_compat.cupy as cp
+#
+#    xp_dev_list.append((cp, cp.cuda.Device(0)))
+#
+# if parallelproj.torch_enabled:
+#    import array_api_compat.torch as torch
+#
+#    xp_dev_list.append((torch, "cpu"))
+#
+#    if parallelproj.cuda_present and parallelproj.cupy_enabled:
+#        xp_dev_list.append((torch, "cuda"))
+
+pytestmark = pytest.mark.parametrize("xp,dev", xp_dev_list)
--- tests/test_nontof_fwd_back.py
+++ tests/test_nontof_fwd_back.py
@@ -1,46 +1,53 @@
 import numpy as np
-import array_api_strict as xp
 import parallelproj_python as pp
-import pytest
+
+from types import ModuleType
+
+from .config import pytestmark
 
 
-@pytest.fixture
-def setup_data():
-    """Fixture to set up test data."""
+def test_forward_and_back_projection(xp: ModuleType, dev: str):
+    """Test the forward projection."""
+
+    ############################################################################
+    # setup data
+
     img_dim = (2, 3, 4)
-    voxsize = xp.asarray([4.0, 3.0, 2.0], dtype=xp.float32)
-    img_origin = (-0.5 * xp.asarray(img_dim, dtype=xp.float32) + 0.5) * voxsize
+    voxsize = xp.asarray([4.0, 3.0, 2.0], dtype=xp.float32, device=dev)
+    img_origin = (
+        -0.5 * xp.asarray(img_dim, dtype=xp.float32, device=dev) + 0.5
+    ) * voxsize
 
     # Read the image from file
-    img = xp.reshape(xp.asarray(np.loadtxt("img.txt", dtype=np.float32)), img_dim)
+    img = xp.reshape(
+        xp.asarray(np.loadtxt("img.txt", dtype=np.float32), device=dev), img_dim
+    )
 
     # Read the ray start and end coordinates from file
     vstart = xp.reshape(
-        xp.asarray(np.loadtxt("vstart.txt", dtype=np.float32)), (2, 5, 3)
+        xp.asarray(np.loadtxt("vstart.txt", dtype=np.float32), device=dev), (2, 5, 3)
     )
-    vend = xp.reshape(xp.asarray(np.loadtxt("vend.txt", dtype=np.float32)), (2, 5, 3))
+    vend = xp.reshape(
+        xp.asarray(np.loadtxt("vend.txt", dtype=np.float32), device=dev), (2, 5, 3)
+    )
 
     # Calculate the start and end coordinates in world coordinates
     xstart = vstart * voxsize + img_origin
     xend = vend * voxsize + img_origin
 
-    return img_dim, voxsize, img_origin, img, xstart, xend
+    # Read the expected forward values from file
+    expected_fwd_vals = xp.reshape(
+        xp.asarray(np.loadtxt("expected_fwd_vals.txt", dtype=np.float32)),
+        xstart.shape[:-1],
+    )
 
-
-def test_forward_and_back_projection(setup_data):
-    """Test the forward projection."""
-    img_dim, voxsize, img_origin, img, xstart, xend = setup_data
+    ############################################################################
 
     # Allocate memory for forward projection results
-    img_fwd = xp.zeros(xstart.shape[:-1], dtype=xp.float32)
+    img_fwd = xp.zeros(xstart.shape[:-1], dtype=xp.float32, device=dev)
 
     # Perform forward projection
     pp.joseph3d_fwd(xstart, xend, img, img_origin, voxsize, img_fwd)
-
-    # Read the expected forward values from file
-    expected_fwd_vals = xp.reshape(
-        xp.asarray(np.loadtxt("expected_fwd_vals.txt", dtype=np.float32)), img_fwd.shape
-    )
 
     # Check if we got the expected results
     eps = 1e-7
@@ -49,8 +56,8 @@ def test_forward_and_back_projection(setup_data):
     ), "Forward projection test failed."
 
     # Allocate memory for back projection results
-    bimg = xp.zeros(img_dim, dtype=xp.float32)
-    ones = xp.ones(xstart.shape[:-1], dtype=xp.float32)
+    bimg = xp.zeros(img_dim, dtype=xp.float32, device=dev)
+    ones = xp.ones(xstart.shape[:-1], dtype=xp.float32, device=dev)
 
     # Perform back projection
     pp.joseph3d_back(xstart, xend, bimg, img_origin, voxsize, ones)
