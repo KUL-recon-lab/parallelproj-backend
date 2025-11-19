a135d847ff2915c91fc540207d13847cd249ebde
Georg Schramm (Windows) <georg.schramm@kuleuven.be>
2025-11-19 12:37:02 +0100

    clean up test config

--- tests/config.py
+++ tests/config.py
@@ -1,37 +1,27 @@
 import pytest
+import importlib
 
-import array_api_strict as nparr
 import array_api_compat.numpy as np
 
 import parallelproj_backend as pp
 
-# generate list of array_api modules / device combinations to test
-
 xp_dev_list = []
 xp_dev_list.append((np, "cpu"))
-xp_dev_list.append((nparr, None))
 
-if pp.PARALLELPROJ_CUDA == 1:
-    try:
-        import array_api_compat.cupy as cp
+if importlib.util.find_spec("array_api_strict") is not None:
+    import array_api_strict as nparr
+    xp_dev_list.append((nparr, None))
 
-        xp_dev_list.append((cp, cp.cuda.Device(0)))
-    except ImportError:
-        pass
-
-try:
+if importlib.util.find_spec("array_api_compat.torch") is not None and importlib.util.find_spec("torch") is not None:
+    torch_available = True
     import array_api_compat.torch as torch
-
     xp_dev_list.append((torch, "cpu"))
-except ImportError:
-    pass
 
 if pp.PARALLELPROJ_CUDA == 1:
-    try:
-        import array_api_compat.torch as torch
-
+    if torch_available:
         xp_dev_list.append((torch, "cuda"))
-    except ImportError:
-        pass
+    if importlib.util.find_spec("array_api_compat.cupy") is not None and importlib.util.find_spec("cupy") is not None:
+        import array_api_compat.cupy as cp
+        xp_dev_list.append((cp, cp.cuda.Device(0)))
 
 pytestmark = pytest.mark.parametrize("xp,dev", xp_dev_list)
